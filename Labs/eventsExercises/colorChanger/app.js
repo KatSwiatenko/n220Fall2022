098af5811f4f5e6c017bda9f5255b1ea3844905c
KatSwiatenko <111912137+KatSwiatenko@users.noreply.github.com>
2022-11-17 18:51:53 -0500

    color changer

--- Labs/eventsExercises/colorChanger/app.js
+++ Labs/eventsExercises/colorChanger/app.js
@@ -1,5 +1,18 @@
 // When div is clicked, it should change color to red, green, or blue
 
-function handleClick(event) {
-    console.log("Test hit");
+let cBox = document.getElementById("cBox1");
+
+function clickRed(event) {
+    event.target.style.backgroundColor = "#900";
+    console.log("Turned red");
+}
+
+function clickGreen(event) {
+    event.target.style.backgroundColor = "#090";
+    console.log("Turned green");
+}
+
+function clickBlue(event) {
+    event.target.style.backgroundColor = "#009";
+    console.log("Turned blue");
 }
