0fc6a9997bb0d1cbfcfc865c30c0042463d7550c
KatSwiatenko <111912137+KatSwiatenko@users.noreply.github.com>
2022-11-17 14:11:12 -0500

    ye

---
+++ Labs/eventsExercises/colorChanger/app.js
@@ -0,0 +1,5 @@
+// When div is clicked, it should change color to red, green, or blue
+
+function handleClick(event) {
+    console.log("Test hit");
+}
---
+++ Labs/lab5/justAvg/app.js
@@ -0,0 +1,12 @@
+/* Create an application that:
+
+1. Takes in a list of comma-separated numbers from the user
+2. Splits that list into an array of numbers
+3. Converts the string version of the numbers into number versions
+4. Loops through the array and sums up the values
+5. Calculates the average of the array
+6. Displays to the user
+7. The average
+8. The sum
+9. Removes the text from the input so the user can type in new numbers  */
+
